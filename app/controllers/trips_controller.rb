26a215e8f0cf16a89201a78583b80d28258d7a2b
Andre Paul Cruz <andrepaulcruz29@gmail.com>
2025-10-13 08:43:24 +0000

    added, edit button, delete button and configured how to change images

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,6 @@
 class TripsController < ApplicationController
+  before_action :load_trip, except: [ :index, :new, :create ]
+
   def index
     @trips = Trip.all
   end
@@ -16,11 +18,43 @@ class TripsController < ApplicationController
   end
 
   def show
-    @trip = Trip.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    # Remove selected images
+    if params[:remove_media_ids]
+      params[:remove_media_ids].each do |id|
+        media = @trip.media.find_by_id(id)
+        media.purge if media
+      end
+    end
+
+    # Attach new images if present
+    if params[:trip][:media].present?
+      @trip.media.attach(params[:trip][:media])
+    end
+
+    if @trip.update(trip_params.except(:media))
+      redirect_to @trip, notice: "Trip was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path, alert: "Trip was successfully deleted."
   end
 
   private
   def trip_params
     params.require(:trip).permit(:title, :destination, :start_date, :end_date, :description, media: [])
+  end
+
+  def load_trip
+    @trip = Trip.find(params[:id])
   end
 end
